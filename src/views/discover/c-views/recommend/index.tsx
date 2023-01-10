fbc69ac12154e1424b48e7db47330648c21b0cb3
lr159151 <1909265384@qq.com>
2023-01-10 23:37:53 +0800

    build: the page of recommend layout

--- src/views/discover/c-views/recommend/index.tsx
+++ src/views/discover/c-views/recommend/index.tsx
@@ -3,6 +3,7 @@ import type { FC, ReactNode } from 'react'
 import { useAppDispatch } from '@/store'
 import { fetchBannerDataAction } from '@/views/discover/c-views/recommend/store'
 import MainBanner from '@/views/discover/c-views/recommend/c-cpns/main-banner'
+import { RecommendWrapper } from '@/views/discover/c-views/recommend/style'
 
 interface IProps {
   children?: ReactNode
@@ -16,9 +17,13 @@ const Recommend: FC<IProps> = () => {
   }, [])
 
   return (
-    <div>
+    <RecommendWrapper>
       <MainBanner />
-    </div>
+      <div className="content wrap-v2">
+        <div className="left">left</div>
+        <div className="right">right</div>
+      </div>
+    </RecommendWrapper>
   )
 }
 
